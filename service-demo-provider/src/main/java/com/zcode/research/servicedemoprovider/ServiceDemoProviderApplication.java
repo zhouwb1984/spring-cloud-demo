eea86156b01349d392355075637102222c9e3077
zhouwb <zhouwenbo1@vipkid.com.cn>
2018-11-13 19:35:48 +0800

    Initial commit

---
+++ service-demo-provider/src/main/java/com/zcode/research/servicedemoprovider/ServiceDemoProviderApplication.java
@@ -0,0 +1,12 @@
+package com.zcode.research.servicedemoprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDemoProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDemoProviderApplication.class, args);
+    }
+}
---
+++ service-demo-provider/src/test/java/com/zcode/research/servicedemoprovider/ServiceDemoProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zcode.research.servicedemoprovider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceDemoProviderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
